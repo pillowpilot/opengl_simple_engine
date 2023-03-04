9c848bf8005853df75d632fda61e468eaf8d76c2
Carlos Federico <carlos.federico005@gmail.com>
2023-03-04 19:09:33 -0300

    feat (fragment shader): Add FragmentShader class

--- src/fragment_shader.hpp
+++ src/fragment_shader.hpp
@@ -9,6 +9,29 @@
 #include "./utils.hpp"
 #include "./shader.hpp"
 
+class FragmentShader: public Shader
+{
+public:
+    FragmentShader(const std::filesystem::path& path)
+    {
+        const auto sourceCode = readFile(path);
+
+        this->id = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(this->id, 1, StringHelper(sourceCode), NULL);
+        glCompileShader(this->id);
+
+        const auto wasCompiled = this->getCompilationStatus();
+        if(!wasCompiled)
+        {
+            spdlog::error("Compilation of shader {} failed", this->id);
+            const auto log = this->getInfoLog();
+            spdlog::info(log);
+        }
+        else
+            spdlog::info("Compilation of shader {} successful", this->id);
+    }
+};
+
 GLuint createFragmentShader();
 
 #endif // __FRAGMENT_SHADER_HPP__
