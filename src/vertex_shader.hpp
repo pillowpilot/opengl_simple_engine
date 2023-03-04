06279abba632a5f423c497adaa6ecd4600cf8e35
Carlos Federico <carlos.federico005@gmail.com>
2023-03-04 19:16:20 -0300

    feat (vertex shader): Add class VertexShader

--- src/vertex_shader.hpp
+++ src/vertex_shader.hpp
@@ -7,6 +7,29 @@
 #include "./utils.hpp"
 #include "./shader.hpp"
 
+class VertexShader: public Shader
+{
+public:
+    VertexShader(const std::filesystem::path& path)
+    {
+        const auto sourceCode = readFile(path);
+
+        this->id = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(this->id, 1, StringHelper(sourceCode), NULL);
+        glCompileShader(this->id);
+
+        const auto wasCompiled = this->getCompilationStatus();
+        if(!wasCompiled)
+        {
+            spdlog::error("Compilation of shader {} failed", this->id);
+            const auto log = this->getInfoLog();
+            spdlog::info(log);
+        }
+        else
+            spdlog::info("Compilation of shader {} successful", this->id);
+    }
+};
+
 GLuint createVertexShader();
 
 #endif // __VERTEX_SHADER_HPP__
