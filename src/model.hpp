e2e6f7efcf137b0df423eee3ce4a2088ef22ca87
Carlos Federico <carlos.federico005@gmail.com>
2023-04-02 01:55:17 -0400

    refactor (model): Remove unused includes

--- src/model.hpp
+++ src/model.hpp
@@ -4,9 +4,6 @@
 #include <vector>
 #include <filesystem>
 
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-#include <glm/glm.hpp>
 #include <assimp/Importer.hpp>
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
